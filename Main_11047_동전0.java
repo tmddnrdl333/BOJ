240837e3c75565562a8ce1d42756e9b93aa60157
SeungNong <62299471+tmddnrdl333@users.noreply.github.com>
2022-03-02 22:48:56 +0900

    Add files via upload

---
+++ Main_11047_동전0.java
@@ -0,0 +1,46 @@
+package study.day0302;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// 11,580kb
+// 80ms
+
+public class Main_11047_동전0 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	//
+	static StringTokenizer st;
+
+	static int N, K, cnt = 0;
+	static int[] arr;
+
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		for (int i = 0; i < N; i++)
+			arr[i] = Integer.parseInt(br.readLine());
+
+		for (int i = N - 1; i >= 0; i--) {
+			if (arr[i] > K)
+				continue;
+			else {
+				cnt += K / arr[i];
+				K -= arr[i] * (K / arr[i]);
+			}
+			if (K == 0)
+				break;
+		}
+
+		bw.write(Integer.toString(cnt));
+		bw.flush();
+		bw.close();
+
+	}
+}
