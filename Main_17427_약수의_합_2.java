04dbb89b6087ea0b0fb877ed49e5a8348f211167
SeungNong <62299471+tmddnrdl333@users.noreply.github.com>
2022-04-28 01:38:55 +0900

    Add files via upload

---
+++ Main_17427_약수의_합_2.java
@@ -0,0 +1,30 @@
+package ing;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// 11,636kb
+// 92ms
+
+public class Main_17427_약수의_합_2 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+
+	static int N;
+	static long res = 0;
+
+	public static void main(String[] args) throws IOException {
+		N = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			res += N / i * i;
+		}
+		bw.write(Long.toString(res));
+		bw.flush();
+		bw.close();
+	}
+}
