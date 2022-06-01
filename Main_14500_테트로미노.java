dcb610544131c88f3ff7da5ee1ddfb4426295ee8
SeungNong <62299471+tmddnrdl333@users.noreply.github.com>
2022-06-02 02:43:13 +0900

    Update Main_14500_테트로미노.java

--- Main_14500_테트로미노.java
+++ Main_14500_테트로미노.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 34020kb
+// 440ms
+
 public class Main_14500_테트로미노 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
