b1ef67b2ab4c813a6db373c7d649d47da4ca7824
SeungNong <62299471+tmddnrdl333@users.noreply.github.com>
2022-06-16 14:57:25 +0900

    Add files via upload

---
+++ Main_2470_두_용액.java
@@ -0,0 +1,52 @@
+package ing;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 31,988kb
+// 292ms
+
+public class Main_2470_두_용액 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+
+	public static void main(String[] args) throws IOException {
+		int N = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(arr);
+		int s = 0, e = N - 1;
+		int sum = arr[s] + arr[e];
+		int res = Integer.MAX_VALUE;
+		int rs = s, re = e;
+		while (s < e) {
+//			System.out.println(arr[s] + " " + arr[e] + " " + sum);
+			int cur = Math.abs(sum);
+			if (cur < res) {
+				res = cur;
+				rs = s;
+				re = e;
+			}
+			if (sum == 0)
+				break;
+			else if (sum > 0) {
+				sum -= arr[e];
+				e--;
+				sum += arr[e];
+			} else {
+				sum -= arr[s];
+				s++;
+				sum += arr[s];
+			}
+		}
+		System.out.println(arr[rs] + " " + arr[re]);
+	}
+}
---
+++ Main_2473_세_용액.java
@@ -0,0 +1,69 @@
+package ing;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 13,968kb
+// 180ms
+
+public class Main_2473_세_용액 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+	static int N;
+	static long[] arr;
+	static final long INF = 3000000001l;
+	static long max = INF;
+	static long ans1, ans2, ans3;
+
+	public static void main(String[] args) throws IOException {
+		N = Integer.parseInt(br.readLine());
+		arr = new long[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++)
+			arr[i] = Long.parseLong(st.nextToken());
+		Arrays.sort(arr);
+
+		for (int i = 0; i < N - 2; i++) {
+			two_solution(i);
+		}
+
+		System.out.println(ans1 + " " + ans2 + " " + ans3);
+
+	}
+
+	public static void two_solution(int first) {
+		int s = first + 1, e = N - 1;
+		long sum = arr[first] + arr[s] + arr[e];
+		long res = INF;
+		int rs = s, re = e;
+		while (s < e) {
+			long cur = Math.abs(sum);
+			if (cur < res) {
+				res = cur;
+				rs = s;
+				re = e;
+			}
+			if (sum == 0)
+				break;
+			else if (sum > 0) {
+				sum -= arr[e];
+				e--;
+				sum += arr[e];
+			} else {
+				sum -= arr[s];
+				s++;
+				sum += arr[s];
+			}
+		}
+		if (max > res) {
+			max = res;
+			ans1 = arr[first];
+			ans2 = arr[rs];
+			ans3 = arr[re];
+		}
+	}
+}
