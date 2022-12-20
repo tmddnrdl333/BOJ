ce6944c03e677b7f83003eaf67174dfab6deb955
SeungNong <62299471+tmddnrdl333@users.noreply.github.com>
2022-12-20 21:31:38 +0900

    Add files via upload

---
+++ Main_2920_음계.java
@@ -0,0 +1,31 @@
+package ing;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// 11,568kb
+// 76ms
+
+public class Main_2920_음계 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+
+	public static void main(String[] args) throws IOException {
+		String str = br.readLine();
+		char init = str.charAt(0);
+		if (init == '1' && str.equals("1 2 3 4 5 6 7 8")) {
+			bw.write("ascending");
+		} else if (init == '8' && str.equals("8 7 6 5 4 3 2 1")) {
+			bw.write("descending");
+		} else {
+			bw.write("mixed");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
