7410b621a15a3792075a54d886e0655a5e723539
SeungNong <62299471+tmddnrdl333@users.noreply.github.com>
2022-12-20 21:22:35 +0900

    Add files via upload

---
+++ Main_11720_숫자의_합.java
@@ -0,0 +1,30 @@
+package ing;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// 11,620kb
+// 84ms
+
+public class Main_11720_숫자의_합 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		int N = Integer.parseInt(br.readLine());
+		char[] inp = br.readLine().toCharArray();
+		int sum = 0;
+		for (char c : inp)
+			sum += (c - '0');
+
+		bw.write(sum + "");
+		bw.flush();
+		bw.close();
+	}
+}
