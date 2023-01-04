35f86fdd89ab0944a647584e1848e98c62c39f5a
SeungNong <62299471+tmddnrdl333@users.noreply.github.com>
2023-01-04 22:58:46 +0900

    PS: 10989

---
+++ Main_10989_수_정렬하기_3.java
@@ -0,0 +1,33 @@
+package ing;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 508,640kb
+// 2,580ms
+
+public class Main_10989_수_정렬하기_3 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++)
+			arr[i] = Integer.parseInt(br.readLine());
+		Arrays.sort(arr);
+		for (int i = 0; i < N; i++)
+			sb.append(arr[i]).append("\n");
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+}
