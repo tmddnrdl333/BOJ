36668fab0d5d912e3cdcdddbd099c885291cb8a2
SeungNong <tmddnrdl333@naver.com>
2023-04-01 09:32:26 +0900

    PS: 25192

---
+++ _00_workspace/Main_25192_인사성_밝은_곰곰이.java
@@ -0,0 +1,45 @@
+package ps.BOJ._00_workspace;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+// 27,516kb
+// 232ms
+
+public class Main_25192_인사성_밝은_곰곰이 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		int N = Integer.parseInt(br.readLine());
+
+		Set<String> set = new HashSet<>();
+		int cnt = 0;
+		String input = br.readLine();
+
+		for (; N > 0; N--) {
+			if (input.equals("ENTER")) {
+				cnt += set.size();
+				set = new HashSet<>();
+			} else {
+				set.add(input);
+			}
+			if (N != 1)
+				input = br.readLine();
+		}
+
+		cnt += set.size();
+
+		bw.write(cnt + "");
+		bw.flush();
+		bw.close();
+	}
+}
