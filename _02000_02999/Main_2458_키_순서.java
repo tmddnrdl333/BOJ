7edbd892bda6a1d6032eac52fcded2d408d9dd55
SeungNong <tmddnrdl333@naver.com>
2023-04-05 10:25:16 +0900

    PS: 2458

---
+++ _02000_02999/Main_2458_키_순서.java
@@ -0,0 +1,59 @@
+package ps.BOJ._02000_02999;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// 35,904kb
+// 568ms
+
+public class Main_2458_키_순서 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		boolean[][] aM = new boolean[N + 1][N + 1];
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			aM[a][b] = true;
+		}
+
+		for (int k = 1; k <= N; k++) {
+			for (int i = 1; i <= N; i++) {
+				if (i == k)
+					continue;
+				for (int j = 1; j <= N; j++) {
+					if (i == j || j == k)
+						continue;
+					if (aM[i][k] && aM[k][j])
+						aM[i][j] = true;
+				}
+			}
+		}
+
+		int res = 0;
+		for (int i = 1; i <= N; i++) {
+			int cnt = 0;
+			for (int j = 1; j <= N; j++) {
+				if (aM[i][j] || aM[j][i])
+					cnt++;
+			}
+			if (cnt == N - 1)
+				res++;
+		}
+
+		bw.write(res + "");
+		bw.flush();
+		bw.close();
+	}
+}
