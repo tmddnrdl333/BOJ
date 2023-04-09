880745b7ff391fe0f7d429e361e0798db860277f
SeungNong <tmddnrdl333@naver.com>
2023-04-09 18:47:58 +0900

    PS: 9655

---
+++ _09000_09999/Main_9655_돌_게임.java
@@ -0,0 +1,25 @@
+package ps.BOJ._09000_09999;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// 11,620kb
+// 76ms
+
+public class Main_9655_돌_게임 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		int N = Integer.parseInt(br.readLine());
+		bw.write(N % 2 == 1 ? "SK" : "CY");
+		bw.flush();
+		bw.close();
+	}
+}
